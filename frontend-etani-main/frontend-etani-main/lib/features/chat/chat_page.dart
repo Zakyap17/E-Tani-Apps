a5f22c616ea0a83e1e1ba9db107b90004aa6f05f
Zakyap17 <zakyaprilian17@gmail.com>
2026-05-15 10:31:36 +0700

    Fix: Add missing geolocator import

--- frontend-etani-main/frontend-etani-main/lib/features/chat/chat_page.dart
+++ frontend-etani-main/frontend-etani-main/lib/features/chat/chat_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:feetani/core/constants/colors.dart';
 import 'package:feetani/core/constants/api_constants.dart';
